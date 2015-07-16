9044fe7d8b4ad8856f447b76386d8b859a7e8d1f
Steve Toro <steve@soverin.net>
2015-07-16 16:46:52 -0400

    Configured environment variables and updated gitignore

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,8 +1,8 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     provider:               'AWS',
-    aws_access_key_id:      ENV['S3_KEY'],
-    aws_secret_access_key:  ENV['S3_SECRET'],
+    aws_access_key_id:      ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key:  ENV['AWS_SECRET_ACCESS_KEY'],
   }
 
   # For testing, upload files to local `tmp` folder.
