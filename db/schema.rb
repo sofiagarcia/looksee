3e6bb6bb9e0879a8e4cb06c22d5087215d03edea
Steve Toro <steve@soverin.net>
2015-07-15 21:26:15 -0400

    heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150716002053) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "images", force: :cascade do |t|
     t.string   "title"
     t.string   "url"
