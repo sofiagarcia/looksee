a9ef58881db622b85497b9d4db878e3a92898768
Sofia Garcia <sofisagarcia@gmail.com>
2015-07-17 11:12:45 -0400

    links home-btn to homepage and explore-btn to image index page

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
 
   get 'twothouten', to: 'images#index10s'
 
+  get 'homepage', to: 'homepage#homepage'
+
+  get 'images', to: "images#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
